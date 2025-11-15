f1e5daa3756a2a5d46a199ec7e42886d09706cc8
slemanz <william_sleman@hotmail.com>
2025-11-15 06:28:47 -0300

    adc(feat): planning the driver

--- fundamentals/adc/main.s
+++ fundamentals/adc/main.s
@@ -8,6 +8,20 @@
 
 __main:
 
+/* adc_init: */
+        /* 1. Enable clock access to ADC pin's GPIO port */
+        /* 2. Set ADC pin, PA as analog pin */
+        /* 3. Enable clock access to the ADC */
+        /* 4. select software trigger */
+        /* 5. Set conversion sequence starting channel */
+        /* 6. Set conversion sequence length */
+        /* Enable adc module */
+
+/* adc_read: */
+        /* 1. Start conversion */
+        /* 2. Wait for the conversion to be complete */
+        /* 3. Read content of ADC data register */
+
 stop:
         b stop
 
