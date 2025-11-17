cd99cc2ff3c68b0a3d9a564e15416d77b3c44660
slemanz <william_sleman@hotmail.com>
2025-11-17 04:53:34 -0300

    adc(feat): call adc functions

--- fundamentals/adc/main.c
+++ fundamentals/adc/main.c
@@ -1,10 +1,21 @@
 #include <stdint.h>
 
+extern void adc_init(void);
+extern uint32_t adc_read(void);
+extern void led_init(void);
+extern void led_control(void);
+
+uint32_t sensor_value;
+
 int main(void)
 {
+    led_init();
+    adc_init();
 
     while(1)
     {
-
+        sensor_value = adc_read();
+        led_control();
+        (void)sensor_value;
     }
 }
