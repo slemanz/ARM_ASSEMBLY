33c3ed3d6bf88f958d40efe11ba69f68020ca324
slemanz <william_sleman@hotmail.com>
2025-11-21 11:19:40 -0300

    systick(feat): add symbolic and gpioa init

--- fundamentals/systick/main.s
+++ fundamentals/systick/main.s
@@ -1,3 +1,11 @@
+.equ NVIC_ST_CTRL_R,        0xE000E010
+.equ NVIC_ST_RELOAD_R,      0xE000E014
+.equ NVIC_ST_CURRENT_R,     0xE000E018
+
+.equ SYSTICK_24BIT_MAX,     0x00FFFFFF
+.equ ST_CTRL_EN,            (1 << 0)
+.equ ST_CTRL_CLKSRC,        (1 << 2)
+.equ ST_CTRL_COUNTFLG,      (1 << 16)
 
 .equ    RCC_BASE,           0x40023800
 .equ    AHB1ENR_OFFSET,     0x30
@@ -28,10 +36,7 @@
 .equ BSRR_5_SET,            (1 << 5)
 .equ BSRR_5_RESET,          (1 << 21)
 
-// active low switch
-.equ    BTN_ON,             0x0000
-.equ    BTN_OFF,            0x0040
-.equ    BTN_PIN,            0x0040
+.equ DELAY1MS,              16000
 
 
         .syntax unified
@@ -42,60 +47,30 @@
         .section .text
         .global __main
 
+/*Default frequency =  16Mhz =>  16 0000 0000 cycles/second
+ 1 millisecond  =  16 0000 */
+
 __main:
-        bl      gpio_init
+        bl      gpioa_init
 
 loop:
-        bl      get_input
-        cmp     r0, #BTN_ON
-        beq     turn_led_on
-        cmp     r0, #BTN_OFF
-        beq     turn_led_off
         b       loop
 
-
-
-
-turn_led_on:
-        mov     r1, #0
-        ldr     r2, =GPIOA_BSRR
-        mov     r1, #BSRR_5_SET
-        str     r1, [r2]
-
-turn_led_off:
-        mov     r1, #0
-        ldr     r2, =GPIOA_BSRR
-        mov     r1, #BSRR_5_RESET
-        str     r1, [r2]
-
-get_input:
-        ldr     r1, =GPIOA_IDR
-        ldr     r0, [r1]
-        and     r0, r0, #BTN_PIN
-        bx      lr
-    
-gpio_init:
-        /* Set PA5 as output */
-        /* ENABLE CLOCK ACCESS TO GPIOA */
+gpioa_init:
+        /* enable clock access to gpioa */
         ldr r0, =RCC_AHB1ENR
-
         ldr r1, [r0]
-
         orr r1, #GPIOA_EN
-
         str r1, [r0]
 
-        // set PA5 as output
+        /* set PA5 as output */
         ldr r0, =GPIOA_MODER
         ldr r1, [r0]
         orr r1, #MODER5_OUT
         str r1, [r0]
 
-        // set PA6 as input
-        ldr r0, =GPIOA_PUPDR
-        ldr r1, [r0]
-        orr r1, #PUPDR6_PU
-        str r1, [r0]
+        mov r1, #0
+        ldr r2, =GPIOA_ODR
         bx  lr
 
 stop:
