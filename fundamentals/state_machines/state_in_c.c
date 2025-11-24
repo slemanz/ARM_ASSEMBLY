9a1aba3760e6ff42a9f1a8d95cd1258e1d3b7aad
slemanz <william_sleman@hotmail.com>
2025-11-24 04:38:56 -0300

    state machines(feat): add defines and state struct

--- fundamentals/state_machines/state_in_c.c
+++ fundamentals/state_machines/state_in_c.c
@@ -1,4 +1,38 @@
-#include <stdint.h>
+#include "stm32f411xx.h"
+
+#define TRAFFIC_LIGHTS_PORT         GPIOA
+#define CAR_SENSOR_PORT             GPIOC
+
+#define EAST_LED_RED                (1U << 9)
+#define EAST_LED_YELLOW             (1U << 8)
+#define EAST_LED_GREEN              (1U << 7)
+
+#define NORTH_LED_RED               (1U << 6)
+#define NORTH_LED_YELLOW            (1U << 5)
+#define NORTH_LED_GREEN             (1U << 4)
+
+#define EAST_LED_RED_MODE_BIT       (1U << 18)
+#define EAST_LED_YELLOW_MODE_BIT    (1U << 16)
+#define EAST_LED_GREEN_MODE_BIT     (1U << 14)
+
+#define NORTH_LED_RED_MODE_BIT      (1U << 18)
+#define NORTH_LED_YELLOW_MODE_BIT   (1U << 16)
+#define NORTH_LED_GREEN_MODE_BIT    (1U << 14)
+
+// go_north, PA9-4   = 100001 makes it green on North and red on East
+// wait_north, PA9-4 = 100010 makes it yellow on North and red on East
+// go_east, PA9-4    = 001100 makes it red on North and green on East
+// wait_east, PA9-4  = 010100 makes it red on North and yellow on East
+
+struct State
+{
+    uint32_t output;
+    uint32_t time;
+    uint8_t next_state[4];
+};
+
+typedef const struct State stateType;
+
 
 int main(void)
 {
